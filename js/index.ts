e1582fdd5ec3b0c22ad5ca55e8bdbd295d51b07a
d9nich <54186666+d9nchik@users.noreply.github.com>
2021-10-13 15:57:26 +0300

    add unhealth node api

--- js/index.ts
+++ js/index.ts
@@ -1,14 +1,42 @@
-import http from 'http';
+import fastify from 'fastify';
+const server = fastify({ logger: true });
 
-const requestListener: http.RequestListener = (req, res) => {
-  if (req.url === '/api/service1') {
-    res.writeHead(200);
-    res.write('Hello from node server');
-  } else {
-    res.writeHead(404);
+let isHealth = true;
+
+// Declare a route
+server.get('/api/service1', async (request, reply) => {
+  if (!isHealth) {
+    await sleep(2000);
   }
-  res.end();
-};
+  reply.status(200);
+  reply.send('Hello from node server!');
+  return;
+});
+server.get('/api/service1/unhealth', (request, reply) => {
+  isHealth = false;
+  reply.status(200);
+  reply.send('Server is unhealth!');
+  // return { hello: 'world' };
+});
+server.get('/api/service1/health', (request, reply) => {
+  isHealth = true;
+  reply.status(200);
+  reply.send('Server is health!');
+  // return { hello: 'world' };
+});
 
-const server = http.createServer(requestListener);
-server.listen(8080);
+// Run the server!
+(async () => {
+  try {
+    await server.listen(8080, '0.0.0.0');
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+})();
+
+function sleep(ms: number): Promise<void> {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
+}
