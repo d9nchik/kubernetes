205966ee65122a1f5bbe9d0a8dd77e3db9634e46
d9nich <54186666+d9nchik@users.noreply.github.com>
2021-10-18 17:18:56 +0300

    add node producer service

--- js/index.ts
+++ js/index.ts
@@ -1,29 +1,88 @@
 import fastify from 'fastify';
 const server = fastify({ logger: true });
 
-let isHealth = true;
-const unhealthTime = 10; // in seconds
+import Kafka from 'node-rdkafka';
 
-// Declare a route
-server.get('/api/service1', async (request, reply) => {
-  if (!isHealth) {
-    await sleep(unhealthTime * 1000);
+interface ChangeBalanceProps {
+  accountID: number;
+  money: number;
+}
+
+interface ChangeBalanceWithOperationProps extends ChangeBalanceProps {
+  type: 'withdraw' | 'deposit';
+}
+
+const stream = Kafka.Producer.createWriteStream(
+  {
+    'metadata.broker.list': 'lab-kafka:9092',
+  },
+  {},
+  { topic: 'change-balance' }
+);
+
+const writeToStream = (value: ChangeBalanceWithOperationProps) => {
+  // Writes a message to the stream
+  const queuedSuccess = stream.write(Buffer.from(JSON.stringify(value)));
+
+  if (queuedSuccess) {
+    console.log('We queued our message!');
+  } else {
+    // Note that this only tells us if the stream's queue is full,
+    // it does NOT tell us if the message got to Kafka!  See below...
+    console.log('Too many messages in our queue already');
   }
-  reply.status(200);
-  reply.send('Hello from node server!');
-  return;
+};
+
+// NOTE: MAKE SURE TO LISTEN TO THIS IF YOU WANT THE STREAM TO BE DURABLE
+// Otherwise, any error will bubble up as an uncaught exception.
+stream.on('error', function (err) {
+  // Here's where we'll know if something went wrong sending to Kafka
+  console.error('Error in our kafka stream');
+  console.error(err);
 });
-server.get('/api/service1/unhealth', (request, reply) => {
-  isHealth = false;
+
+// SERVER implementation
+server.post('/withdraw', (request, reply) => {
+  const body = request.body as ChangeBalanceProps;
   reply.status(200);
-  reply.send('Server is unhealth!');
-  // return { hello: 'world' };
+
+  if (body.money <= 0) {
+    reply.send('Money can not be negative');
+    return;
+  }
+
+  const changeBalanceWithOperation: ChangeBalanceWithOperationProps = {
+    ...body,
+    type: 'withdraw',
+  };
+
+  writeToStream(changeBalanceWithOperation);
+
+  reply.send('Your operation will be processed soon!');
 });
-server.get('/api/service1/health', (request, reply) => {
-  isHealth = true;
+
+server.post('/deposit', (request, reply) => {
+  const body = request.body as ChangeBalanceProps;
   reply.status(200);
-  reply.send('Server is health!');
-  // return { hello: 'world' };
+
+  if (body.money <= 0) {
+    reply.send('Money can not be negative');
+    return;
+  }
+
+  const changeBalanceWithOperation: ChangeBalanceWithOperationProps = {
+    ...body,
+    type: 'deposit',
+  };
+
+  writeToStream(changeBalanceWithOperation);
+
+  reply.send('Your operation will be processed soon!');
+});
+
+server.post('/balance', (request, reply) => {
+  reply.status(200);
+  reply.send('Will be implemented soon!');
 });
 
 // Run the server!
@@ -35,9 +94,3 @@ server.get('/api/service1/health', (request, reply) => {
     process.exit(1);
   }
 })();
-
-function sleep(ms: number): Promise<void> {
-  return new Promise(resolve => {
-    setTimeout(resolve, ms);
-  });
-}
