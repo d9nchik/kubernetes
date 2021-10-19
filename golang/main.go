4d2388115c8484510d5be8caac6312bf2d8c8313
d9nich <54186666+d9nchik@users.noreply.github.com>
2021-10-19 15:05:44 +0300

    add consumer group to go

--- golang/main.go
+++ golang/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"flag"
 	"log"
 	"os"
 	"os/signal"
+	"strings"
+	"sync"
+	"syscall"
 
 	"github.com/Shopify/sarama"
 	"github.com/jackc/pgx/v4"
@@ -17,86 +21,193 @@ type ChangeBalanceStruct struct {
 	TypeOperation string `json:"type"`
 }
 
+var (
+	brokers  = ""
+	version  = ""
+	group    = ""
+	topics   = ""
+	assignor = ""
+	oldest   = true
+	verbose  = false
+)
+
+func init() {
+	flag.StringVar(&brokers, "brokers", "", "Kafka bootstrap brokers to connect to, as a comma separated list")
+	flag.StringVar(&group, "group", "go-group", "Kafka consumer group definition")
+	flag.StringVar(&version, "version", "2.1.1", "Kafka cluster version")
+	flag.StringVar(&topics, "topics", "change-balance", "Kafka topics to be consumed, as a comma separated list")
+	flag.StringVar(&assignor, "assignor", "range", "Consumer group partition assignment strategy (range, roundrobin, sticky)")
+	flag.BoolVar(&oldest, "oldest", true, "Kafka consumer consume initial offset from oldest")
+	flag.BoolVar(&verbose, "verbose", false, "Sarama logging")
+	flag.Parse()
+
+	if len(brokers) == 0 {
+		brokers = os.Getenv("KAFKA_URL")
+	}
+
+	if len(brokers) == 0 {
+		panic("no Kafka bootstrap brokers defined, please set the -brokers flag")
+	}
+
+	if len(topics) == 0 {
+		panic("no topics given to be consumed, please set the -topics flag")
+	}
+
+	if len(group) == 0 {
+		panic("no Kafka consumer group defined, please set the -group flag")
+	}
+}
+
 func main() {
-	// KAFKA URL should be in format address:9092
-	consumer, err := sarama.NewConsumer([]string{os.Getenv("KAFKA_URL")}, sarama.NewConfig())
-	if err != nil {
-		panic(err)
+	log.Println("Starting a new Sarama consumer")
+
+	if verbose {
+		sarama.Logger = log.New(os.Stdout, "[sarama] ", log.LstdFlags)
 	}
 
-	defer func() {
-		if err := consumer.Close(); err != nil {
-			log.Fatalln(err)
+	version, err := sarama.ParseKafkaVersion(version)
+	if err != nil {
+		log.Panicf("Error parsing Kafka version: %v", err)
+	}
+
+	/**
+	 * Construct a new Sarama configuration.
+	 * The Kafka cluster version has to be defined before the consumer/producer is initialized.
+	 */
+	config := sarama.NewConfig()
+	config.Version = version
+
+	switch assignor {
+	case "sticky":
+		config.Consumer.Group.Rebalance.Strategy = sarama.BalanceStrategySticky
+	case "roundrobin":
+		config.Consumer.Group.Rebalance.Strategy = sarama.BalanceStrategyRoundRobin
+	case "range":
+		config.Consumer.Group.Rebalance.Strategy = sarama.BalanceStrategyRange
+	default:
+		log.Panicf("Unrecognized consumer group partition assignor: %s", assignor)
+	}
+
+	if oldest {
+		config.Consumer.Offsets.Initial = sarama.OffsetOldest
+	}
+
+	/**
+	 * Setup a new Sarama consumer group
+	 */
+	consumer := Consumer{
+		ready: make(chan bool),
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	client, err := sarama.NewConsumerGroup(strings.Split(brokers, ","), group, config)
+	if err != nil {
+		log.Panicf("Error creating consumer group client: %v", err)
+	}
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			// `Consume` should be called inside an infinite loop, when a
+			// server-side rebalance happens, the consumer session will need to be
+			// recreated to get the new claims
+			if err := client.Consume(ctx, strings.Split(topics, ","), &consumer); err != nil {
+				log.Panicf("Error from consumer: %v", err)
+			}
+			// check if context was cancelled, signaling that the consumer should stop
+			if ctx.Err() != nil {
+				return
+			}
+			consumer.ready = make(chan bool)
 		}
 	}()
 
-	partitionConsumer, err := consumer.ConsumePartition("change-balance", 0, sarama.OffsetNewest)
-	if err != nil {
-		panic(err)
+	<-consumer.ready // Await till the consumer has been set up
+	log.Println("Sarama consumer up and running!...")
+
+	sigterm := make(chan os.Signal, 1)
+	signal.Notify(sigterm, syscall.SIGINT, syscall.SIGTERM)
+	select {
+	case <-ctx.Done():
+		log.Println("terminating: context cancelled")
+	case <-sigterm:
+		log.Println("terminating: via signal")
 	}
+	cancel()
+	wg.Wait()
+	if err = client.Close(); err != nil {
+		log.Panicf("Error closing client: %v", err)
+	}
+}
 
-	defer func() {
-		if err := partitionConsumer.Close(); err != nil {
-			log.Fatalln(err)
-		}
-	}()
+// Consumer represents a Sarama consumer group consumer
+type Consumer struct {
+	ready chan bool
+	conn  pgx.Conn
+}
 
-	// Trap SIGINT to trigger a shutdown.
-	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, os.Interrupt)
+// Setup is run at the beginning of a new session, before ConsumeClaim
+func (consumer *Consumer) Setup(sarama.ConsumerGroupSession) error {
 	// urlExample := "postgres://username:password@localhost:5432/database_name"
 	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
-
 	if err != nil {
 		log.Fatalf("Unable to connect to database: %v\n", err)
 		os.Exit(1)
 	}
-	defer conn.Close(context.Background())
+	consumer.conn = *conn
+	// Mark the consumer as ready
+	close(consumer.ready)
+	return nil
+}
 
-	log.Println("Service started successfully")
+// Cleanup is run at the end of a session, once all ConsumeClaim goroutines have exited
+func (consumer *Consumer) Cleanup(sarama.ConsumerGroupSession) error {
+	consumer.conn.Close(context.Background())
+	return nil
+}
 
-	consumed := 0
-ConsumerLoop:
-	for {
-		select {
-		case msg := <-partitionConsumer.Messages():
-			log.Printf("Get new message: %v8\n", msg.Value)
+// ConsumeClaim must start a consumer loop of ConsumerGroupClaim's Messages().
+func (consumer *Consumer) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+	// NOTE:
+	// Do not move the code below to a goroutine.
+	// The `ConsumeClaim` itself is called within a goroutine, see:
+	// https://github.com/Shopify/sarama/blob/main/consumer_group.go#L27-L29
+	for message := range claim.Messages() {
+		log.Printf("Message claimed: value = %s, timestamp = %v, topic = %s", string(message.Value), message.Timestamp, message.Topic)
 
-			var data ChangeBalanceStruct
-			err := json.Unmarshal(msg.Value, &data)
-			if err != nil {
-				log.Fatalf("Marshall error: %v\n", err)
-				continue
-			}
-			if data.TypeOperation == "withdraw" {
-				data.Money = -data.Money
-			} else if data.TypeOperation != "deposit" {
-				log.Fatalln("Unknown operation type")
-				continue
-			}
-
-			var balance int
-			err = conn.QueryRow(context.Background(), "SELECT balance FROM bank_accounts WHERE id=$1", data.AccountID).Scan(&balance)
-			if err != nil {
-				log.Fatalln("SELECT balance is unsuccessfull")
-				continue
-			}
-			balance = balance + data.Money
-			if balance < 0 {
-				log.Fatalln("Illegal withdraw")
-				continue
-			}
-			_, err = conn.Exec(context.Background(), "UPDATE bank_accounts SET balance= WHERE id=$1;", data.AccountID, balance)
-			if err != nil {
-				log.Fatalln("Update failed")
-				continue
-			}
-			log.Printf("Consumed message offset %d\n", msg.Offset)
-			consumed++
-		case <-signals:
-			break ConsumerLoop
+		var data ChangeBalanceStruct
+		err := json.Unmarshal(message.Value, &data)
+		if err != nil {
+			log.Fatalf("Marshall error: %v\n", err)
+			continue
 		}
+		if data.TypeOperation == "withdraw" {
+			data.Money = -data.Money
+		} else if data.TypeOperation != "deposit" {
+			log.Fatalln("Unknown operation type")
+			continue
+		}
+		var balance int
+		err = consumer.conn.QueryRow(context.Background(), "SELECT balance FROM bank_accounts WHERE id=$1", data.AccountID).Scan(&balance)
+		if err != nil {
+			log.Fatalln("SELECT balance is unsuccessfull")
+			continue
+		}
+		balance = balance + data.Money
+		if balance < 0 {
+			log.Fatalln("Illegal withdraw")
+			continue
+		}
+		_, err = consumer.conn.Exec(context.Background(), "UPDATE bank_accounts SET balance= WHERE id=$1;", data.AccountID, balance)
+		if err != nil {
+			log.Fatalln("Update failed")
+			continue
+		}
+		log.Printf("Consumed message offset %d\n", message.Offset)
+		session.MarkMessage(message, "")
 	}
 
-	log.Printf("Consumed: %d\n", consumed)
+	return nil
 }
