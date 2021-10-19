e7fc1f83a9cff8221a109a0af88e3d6f806d23df
d9nich <54186666+d9nchik@users.noreply.github.com>
2021-10-19 19:26:21 +0300

    add kafka to python

--- python/app.py
+++ python/app.py
@@ -1,9 +1,73 @@
+import json
 from flask import Flask
+from os import getenv
+from confluent_kafka import Consumer, KafkaException
+import sys
+import logging
+
+income = 0
+outcome = 0
+
+kafka_url = getenv('KAFKA_URL')
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Income: {}<br>Outcome: {}</p>".format(income, outcome)
 
 
+if __name__ == '__main__':
+    broker = kafka_url
+    group = 'python'
+    topics = 'successful-operations'
+    # Consumer configuration
+    # See https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
+    conf = {'bootstrap.servers': broker, 'group.id': group, 'session.timeout.ms': 6000,
+            'auto.offset.reset': 'earliest'}
+
+    # Create logger for consumer (logs will be emitted when poll() is called)
+    logger = logging.getLogger('consumer')
+    logger.setLevel(logging.DEBUG)
+    handler = logging.StreamHandler()
+    handler.setFormatter(logging.Formatter(
+        '%(asctime)-15s %(levelname)-8s %(message)s'))
+    logger.addHandler(handler)
+
+    # Create Consumer instance
+    # Hint: try debug='fetch' to generate some log messages
+    c = Consumer(conf, logger=logger)
+
+    def print_assignment(consumer, partitions):
+        print('Assignment:', partitions)
+
+    # Subscribe to topics
+    c.subscribe(topics, on_assign=print_assignment)
+
+    # Read messages from Kafka, print to stdout
+    try:
+        while True:
+            msg = c.poll(timeout=1.0)
+            if msg is None:
+                continue
+            if msg.error():
+                raise KafkaException(msg.error())
+            else:
+                # Proper message
+                sys.stderr.write('%% %s [%d] at offset %d with key %s:\n' %
+                                 (msg.topic(), msg.partition(), msg.offset(),
+                                  str(msg.key())))
+                print(msg.value())
+                data = json.loads(msg.value())
+                if data['type'] == 'deposit':
+                    income += data['money']
+                else:
+                    outcome += data['money']
+
+    except KeyboardInterrupt:
+        sys.stderr.write('%% Aborted by user\n')
+
+    finally:
+        # Close down consumer to commit final offsets.
+        c.close()
